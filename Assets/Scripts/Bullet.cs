1608f47b26b1bd7b1822062e02ae2aee1847409b
Marcos Gabriel Barreto <marcosgabrielbarreto5@gmail.com>
2024-01-11 20:41:44 -0300

    criação do projetil   e disparo

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -2,13 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Claw : MonoBehaviour
+public class Bullet : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
         GetComponent<Rigidbody2D>().AddForce(transform.right * 1000);
-        Destroy(gameObject, 2);   
+        Destroy(gameObject, 2); 
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Enenmy"))
+        {
+
+        }
+
+        Destroy(gameObject);
     }
     
 }
---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    int side = -1;
+    public Transform bullet;
+    public Transform pivot;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+            side = 1;
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+            side = -1;
+ 
+        transform.localScale = Vector2.right * side;
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Transform obj = Instantiate(bullet, pivot.position, transform.rotation);
+            obj.right = Vector2.right * side;
+        }
+    }
+}
